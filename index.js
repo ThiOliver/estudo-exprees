fc8f99e04cf2d7650144100d06f3ca40665e9d0f
thiOliver <thiago.oliver.siqueira@gmail.com>
2022-04-07 12:01:08 -0300

    feat: adicionando-as-views

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const res = require("express/lib/response")
+const path = require("path")
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views/index.html')))
+
+app.listen(port, () => console.info(`Servidor está rodando na porta ${port}`))
