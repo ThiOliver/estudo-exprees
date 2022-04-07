fcf663cd3a7f45f4ff96d7aa4306280b4700b97d
thiOliver <thiago.oliver.siqueira@gmail.com>
2022-04-07 12:13:21 -0300

    feat: adicionando a rota ea response para rota /sobre

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views/index.html')))
-
+app.get('/Sobre', (req, res) => res.sendFile(path.join(__dirname, 'views/sobre.html')))
 app.listen(port, () => console.info(`Servidor está rodando na porta ${port}`))
